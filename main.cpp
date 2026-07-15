53204c5e4966cf277ab168ea242a48494f190b37
sionaiprod <kalashnikovsergey0303@gmail.com>
2026-07-15 15:42:21 +0300

    first_commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    // Variable to store user input
+    std::string name;
+
+    // Print message to the console
+    std::cout << "Enter your name: ";
+    
+    // Read input from the user
+    std::getline(std::cin, name);
+
+    // Print personalized greeting
+    std::cout << "Hello, " << name << "!" << std::endl;
+
+    // Signal that the program ran successfully
+    return 0;
+}
